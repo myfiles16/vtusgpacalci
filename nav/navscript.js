b9511f98d56162ec522ddd00ea963701b53bb44c
rahulm0160 <161876582+rahulm0160@users.noreply.github.com>
2024-03-08 20:43:22 +0530

    Second commit
    Navigation bar logic

---
+++ nav/navscript.js
@@ -0,0 +1,27 @@
+function redirectToPage(url) {
+    window.location.href = url;
+  }
+  
+  function openNav() {
+    var sidepanel = document.getElementById("mySidepanel");
+    sidepanel.classList.add("initial", "shaped");
+  }
+  
+  function closeNav() {
+    var sidepanel = document.getElementById("mySidepanel");
+    sidepanel.classList.remove("initial", "shaped");
+  }
+  
+  function toggleDarkMode() {
+    var button = document.querySelector('.dark');
+    
+    // Toggle between sun and moon emoji
+    if (button.innerHTML === '🌙') {
+      button.innerHTML = '☀️';
+      // You can also add a class to the body or other elements to apply dark mode styles
+      document.body.classList.add('dark-mode');
+    } else {
+      button.innerHTML = '🌙';
+      document.body.classList.remove('dark-mode');
+    }
+  }
